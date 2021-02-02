26d80f7e61a858be82f018b75d51e0da52f2bf43
Yuriy Mokhov <mokhov86@yandex.ru>
2021-02-02 20:23:38 +0300

    BinarySearch

---
+++ 01_BinarySearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrokkingAlgorithms
+{
+    public class BinarySearch
+    {
+        //11
+        // 1,2,3,4,5,6,7,8,  9,10,11,12, 13,14,15,16
+        public void Search(List<int> sortedList, int searchingNumber)
+        {
+        
+            var low = sortedList[0];
+            var high = sortedList[sortedList.Count - 1];
+            bool isSearched = false;
+            int inerationCount = 0;
+            while (!isSearched)
+            {
+                inerationCount++;
+                int delitel = (int)Math.Truncate((double)(high + low) / 2 + 1); 
+                if (delitel == searchingNumber)
+                {
+                    isSearched = true;
+                }
+
+                if (searchingNumber > delitel)
+                {
+                    low = delitel;
+                }
+                else
+                {
+                    high = delitel - 1; 
+                }
+
+            }
+
+    
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GrokkingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearch bs = new BinarySearch();
+            bs.Search(new System.Collections.Generic.List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17 }, 2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
